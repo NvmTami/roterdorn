ecfedbf37a245ae13fb077b8dee47ce66148888b
RoterDorn Dev <epicmuster2000@gmail.com>
2026-05-05 15:07:42 +0200

    fix(docker): change MySQL host port to 3307

--- backend/config.py
+++ backend/config.py
@@ -2,6 +2,7 @@ import os
 
 DB_CONFIG = {
     "host":     os.environ.get("DB_HOST", "localhost"),
+    "port":     int(os.environ.get("DB_PORT", 3307)),
     "user":     os.environ.get("DB_USER", "root"),
     "password": os.environ.get("DB_PASSWORD", ""),
     "database": os.environ.get("DB_NAME", "roterdorn"),
